ea6357ed6f154fad193437f24411a944b8716294
Sandro Simas <sandro.csimas@gmail.com>
2018-04-03 14:36:06 -0300

    Improving publish task

--- tasks/publish.js
+++ tasks/publish.js
@@ -7,14 +7,14 @@ const Promise = require('bluebird');
 const WORKING_DIR = path.resolve(__dirname, '../');
 const GITHUB_REPOSITORY_NAME = 'ayro-wordpress';
 const GITHUB_REPOSITORY_OWNER = 'ayrolabs';
-const WORDPRESS_REPOSITORY_USERNAME = 'ayrolabs';
-const WORDPRESS_REPOSITORY_URL = 'https://plugins.svn.wordpress.org/ayro';
-const WORDPRESS_REPOSITORY_DIR = 'ayro-wordpress-svn';
-const WORDPRESS_ASSETS_REPOSITORY_DIR = 'ayro-wordpress-svn-assets';
+const WP_REPOSITORY_USERNAME = 'ayrolabs';
+const WP_REPOSITORY_URL = 'https://plugins.svn.wordpress.org/ayro';
+const WP_TRUNK_REPOSITORY_NAME = 'ayro-wordpress-svn-trunk';
+const WP_ASSETS_REPOSITORY_NAME = 'ayro-wordpress-svn-assets';
 const TEMP_DIR = '/tmp';
-const TEMP_GITHUB_REPOSITORY_DIR = `${TEMP_DIR}/${GITHUB_REPOSITORY_NAME}`;
-const TEMP_WORDPRESS_REPOSITORY_DIR = `${TEMP_DIR}/${WORDPRESS_REPOSITORY_DIR}`;
-const TEMP_WORDPRESS_ASSETS_REPOSITORY_DIR = `${TEMP_DIR}/${WORDPRESS_ASSETS_REPOSITORY_DIR}`;
+const TEMP_GITHUB_REPOSITORY_DIR = path.join(TEMP_DIR, GITHUB_REPOSITORY_NAME);
+const TEMP_WP_TRUNK_REPOSITORY_DIR = path.join(TEMP_DIR, WP_TRUNK_REPOSITORY_NAME);
+const TEMP_WP_ASSETS_REPOSITORY_DIR = path.join(TEMP_DIR, WP_ASSETS_REPOSITORY_NAME);
 
 const gitHubApi = new GitHubApi();
 gitHubApi.authenticate({
@@ -32,9 +32,9 @@ function buildPlugin() {
 function prepareGithubRepository() {
   return Promise.coroutine(function* () {
     commands.log('Preparing Github repository...');
-    yield commands.exec(`rm -rf ${GITHUB_REPOSITORY_DIR}`, TEMP_DIR);
+    yield commands.exec(`rm -rf ${GITHUB_REPOSITORY_NAME}`, TEMP_DIR);
     yield commands.exec(`git clone git@github.com:${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY_NAME}.git ${GITHUB_REPOSITORY_NAME}`, TEMP_DIR);
-    yield commands.exec(`rm -rf ${GITHUB_REPOSITORY_DIR}/*`, TEMP_DIR);
+    yield commands.exec(`rm -rf ${GITHUB_REPOSITORY_NAME}/*`, TEMP_DIR);
   })();
 }
 
@@ -78,61 +78,61 @@ function beforePublish() {
   })();
 }
 
-function prepareSvnRepository() {
+function prepareSvnTrunkRepository() {
   return Promise.coroutine(function* () {
-    commands.log('Preparing Subversion repository...');
-    yield commands.exec(`rm -rf ${WORDPRESS_REPOSITORY_DIR}`, TEMP_DIR);
-    yield commands.exec(`svn checkout ${WORDPRESS_REPOSITORY_URL}/trunk ${WORDPRESS_REPOSITORY_DIR}`, TEMP_DIR);
-    yield commands.exec(`rm -rf ${WORDPRESS_REPOSITORY_DIR}/*`, TEMP_DIR);
+    commands.log('Preparing Subversion trunk repository...');
+    yield commands.exec(`rm -rf ${WP_TRUNK_REPOSITORY_NAME}`, TEMP_DIR);
+    yield commands.exec(`svn checkout ${WP_REPOSITORY_URL}/trunk ${WP_TRUNK_REPOSITORY_NAME}`, TEMP_DIR);
+    yield commands.exec(`rm -rf ${WP_TRUNK_REPOSITORY_NAME}/*`, TEMP_DIR);
   })();
 }
 
-function copySvnFiles() {
+function copySvnTrunkFiles() {
   return Promise.coroutine(function* () {
-    commands.log('Copying files to Subversion repository...');
+    commands.log('Copying files to Subversion trunk repository...');
     yield commands.exec(`unzip dist/ayro-wordpress.zip -d ${TEMP_DIR}`);
-    yield commands.exec(`mv ayro/* ${WORDPRESS_REPOSITORY_DIR}`, TEMP_DIR);
+    yield commands.exec(`mv ayro/* ${WP_TRUNK_REPOSITORY_NAME}`, TEMP_DIR);
     yield commands.exec(`rm -Rf ayro `, TEMP_DIR);
   })();
 }
 
-function pushSvnFiles() {
+function pushSvnTrunkFiles() {
   return Promise.coroutine(function* () {
-    commands.log('Committing and pushing files to Subversion repository...');
-    yield commands.exec('svn add *', TEMP_WORDPRESS_REPOSITORY_DIR);
-    yield commands.exec(`svn commit --force-interactive --username ${WORDPRESS_REPOSITORY_USERNAME} -m 'Release ${packageJson.version}'`, TEMP_WORDPRESS_REPOSITORY_DIR);
+    commands.log('Committing and pushing files to Subversion trunk repository...');
+    yield commands.exec('svn add *', TEMP_WP_TRUNK_REPOSITORY_DIR);
+    yield commands.exec(`svn commit --force-interactive --username ${WP_REPOSITORY_USERNAME} -m 'Release ${packageJson.version}'`, TEMP_WP_TRUNK_REPOSITORY_DIR);
   })();
 }
 
 function prepareSvnAssetsRepository() {
   return Promise.coroutine(function* () {
     commands.log('Preparing Subversion assets repository...');
-    yield commands.exec(`rm -rf ${WORDPRESS_ASSETS_REPOSITORY_DIR}`, TEMP_DIR);
-    yield commands.exec(`svn checkout ${WORDPRESS_REPOSITORY_URL}/assets ${WORDPRESS_ASSETS_REPOSITORY_DIR}`, TEMP_DIR);
-    yield commands.exec(`rm -rf ${WORDPRESS_ASSETS_REPOSITORY_DIR}/*`, TEMP_DIR);
+    yield commands.exec(`rm -rf ${WP_ASSETS_REPOSITORY_NAME}`, TEMP_DIR);
+    yield commands.exec(`svn checkout ${WP_REPOSITORY_URL}/assets ${WP_ASSETS_REPOSITORY_NAME}`, TEMP_DIR);
+    yield commands.exec(`rm -rf ${WP_ASSETS_REPOSITORY_NAME}/*`, TEMP_DIR);
   })();
 }
 
 function copySvnAssetsFiles() {
   return Promise.coroutine(function* () {
     commands.log('Copying files to Subversion assets repository...');
-    yield commands.exec(`cp assets/* ${TEMP_WORDPRESS_ASSETS_REPOSITORY_DIR}`);
+    yield commands.exec(`cp assets/* ${TEMP_WP_ASSETS_REPOSITORY_DIR}`);
   })();
 }
 
 function pushSvnAssetsFiles() {
   return Promise.coroutine(function* () {
     commands.log('Committing and pushing files to Subversion assets repository...');
-    yield commands.exec('svn add *', TEMP_WORDPRESS_ASSETS_REPOSITORY_DIR);
-    yield commands.exec(`svn commit --force-interactive --username ${WORDPRESS_REPOSITORY_USERNAME} -m 'Release ${packageJson.version}'`, TEMP_WORDPRESS_ASSETS_REPOSITORY_DIR);
+    yield commands.exec('svn add *', TEMP_WP_ASSETS_REPOSITORY_DIR);
+    yield commands.exec(`svn commit --force-interactive --username ${WP_REPOSITORY_USERNAME} -m 'Release ${packageJson.version}'`, TEMP_WP_ASSETS_REPOSITORY_DIR);
   })();
 }
 
 function publishToWordPressSvn() {
   return Promise.coroutine(function* () {
-    yield prepareSvnRepository();
-    yield copySvnFiles();
-    yield pushSvnFiles();
+    yield prepareSvnTrunkRepository();
+    yield copySvnTrunkFiles();
+    yield pushSvnTrunkFiles();
     yield prepareSvnAssetsRepository();
     yield copySvnAssetsFiles();
     yield pushSvnAssetsFiles();
