1938d0adc309fddf0d39c2ec67b56b3506c5dde4
Sandro Simas <sandro.csimas@gmail.com>
2018-05-21 21:33:59 -0300

    Upgrading ayro library

--- src/ayro.php
+++ src/ayro.php
@@ -32,7 +32,7 @@ if (!defined('WPINC')) {
  */
 define('AYRO_PLUGIN_NAME', 'ayro');
 define('AYRO_PLUGIN_VERSION', '0.0.23');
-define('AYRO_LIBRARY_VERSION', '0.0.34');
+define('AYRO_LIBRARY_VERSION', '0.0.36');
 
 /**
  * The code that runs during plugin activation.
