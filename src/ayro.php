a33079a239a2300a648171fc3a50a0308d582c2c
Sandro Simas <sandro.csimas@gmail.com>
2018-06-03 03:01:48 -0300

    Updating library version

--- src/ayro.php
+++ src/ayro.php
@@ -32,7 +32,7 @@ if (!defined('WPINC')) {
  */
 define('AYRO_PLUGIN_NAME', 'ayro');
 define('AYRO_PLUGIN_VERSION', '0.0.26');
-define('AYRO_LIBRARY_VERSION', '0.0.39');
+define('AYRO_LIBRARY_VERSION', '0.0.40');
 
 /**
  * The code that runs during plugin activation.
