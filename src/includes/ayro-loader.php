8bfdafa16420fbc23a99d5d08942fa48c3ab629d
Sandro Simas <sandro.csimas@gmail.com>
2018-08-17 19:19:40 -0300

    Fixing some wrong spaces

--- src/includes/ayro-loader.php
+++ src/includes/ayro-loader.php
@@ -83,8 +83,8 @@ class AyroLoader {
     foreach ($this->filters as $hook) {
       add_filter($hook['hook'], array($hook['component'], $hook['callback']), $hook['priority'], $hook['accepted_args']);
     }
-    foreach ( $this->actions as $hook) {
-      add_action($hook['hook'], array( $hook['component'], $hook['callback']), $hook['priority'], $hook['accepted_args']);
+    foreach ($this->actions as $hook) {
+      add_action($hook['hook'], array($hook['component'], $hook['callback']), $hook['priority'], $hook['accepted_args']);
     }
   }
 }
